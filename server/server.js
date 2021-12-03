fae56d6649c6f61693d28a315904aacb7c776b88
Apurv011 <apurvdak@gmail.com>
2021-12-03 23:16:32 +0530

    added a Procfile

--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
 const http = require('http');
 const app = require('./app');
 
-const port = process.env.PORT || 5000;
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 5000;
+}
 
 const server = http.createServer(app);
 
